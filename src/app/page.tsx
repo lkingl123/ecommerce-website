85a33b9b58711b9be870e333f0c829f0e3e62c7f
Loke, Jake <Jake.Loke.ctr@schools.utah.gov>
2025-02-26 09:18:29 -0700

    removed search params from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { Suspense } from "react";
 import Hero from "./components/Hero";
+
 
 export default function Home() {
   return (
     <div>
-      {/* ✅ Wrap Hero in Suspense to fix useSearchParams() error */}
-      <Suspense fallback={<div className="flex justify-center py-10"></div>}>
-        <Hero />
-      </Suspense>
+      <Hero />
     </div>
   );
 }
